55ad692d2aef5f11a15a639dceaf81c95b877e4c
임준영 <limejuny@gmail.com>
2021-12-28 01:11:28 +0900

    Add server mods
    Detail
    ======
     - Campfire Respawn
     - Don't Drop Everything
     - Global Positions
     - Health Info
     - Simple Health Bar

---
+++ mods/dedicated_server_mods_setup.lua
@@ -0,0 +1,28 @@
+--There are two functions that will install mods, ServerModSetup and ServerModCollectionSetup. Put the calls to the functions in this file and they will be executed on boot.
+
+--ServerModSetup takes a string of a specific mod's Workshop id. It will download and install the mod to your mod directory on boot.
+	--The Workshop id can be found at the end of the url to the mod's Workshop page.
+	--Example: http://steamcommunity.com/sharedfiles/filedetails/?id=350811795
+	--ServerModSetup("350811795")
+
+--ServerModCollectionSetup takes a string of a specific mod's Workshop id. It will download all the mods in the collection and install them to the mod directory on boot.
+	--The Workshop id can be found at the end of the url to the collection's Workshop page.
+	--Example: http://steamcommunity.com/sharedfiles/filedetails/?id=379114180
+	--ServerModCollectionSetup("379114180")
+
+-- Campfire Respawn
+ServerModSetup("569043634")
+
+-- Don't Drop Everything
+ServerModSetup("661253977")
+
+-- Global Positions
+ServerModSetup("378160973")
+
+-- Health Info
+ServerModSetup("375859599")
+
+-- Simple Health Bar
+ServerModSetup("1207269058")
+
+
