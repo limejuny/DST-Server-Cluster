d61db3f215ae885ca806372e58e5d3e884e27a3d
임준영 <limejuny@gmail.com>
2021-12-28 01:24:42 +0900

    Caves' worldgenoverride.lua
    Detail
    ======
     - Caves' worldgenoverride.lua

--- Caves/worldgenoverride.lua
+++ Caves/worldgenoverride.lua
@@ -1,4 +1,56 @@
 return {
     override_enabled = true,
-    preset = "DST_CAVE",
+    preset = "DST_CAVE", --options are "DST_CAVE", "DST_CAVE_PLUS"
+    overrides = {
+        --MISC
+        world_size="default", --options are "small", "medium", "default", "huge"
+        branching="default", --options are "never", "least", "default", "most"
+        loop="default", --options are "never", "default", "always"
+        weather="default", --options are "never", "rare", "default", "often", "always"
+        earthquakes="default", --options are "never", "rare", "default", "often", "always"
+        touchstone="default", --options are "never", "rare", "default", "often", "always"
+        regrowth="default", --options are "veryslow", "slow", "default", "fast", "veryfast"
+        boons="default", --options are "never", "rare", "default", "often", "always"
+        cavelight="default", --options are "veryslow", "slow", "default", "fast", "veryfast"
+
+        disease_delay="default", --options are "none", "random", "long", "default", "short"
+        prefabswaps_start="default", --options are "classic", "default", "highly random"
+        prefabswaps="default", --options are "default", "none", "few", "normal", "many", "max"
+
+        --RESOURCES --options are "never", "rare", "default", "often", "always"
+        grass="default",
+        sapling="default",
+        marshbush="default",
+        reeds="default",
+        trees="default",
+        flint="default",
+        rock="default",
+        mushtree="rare",
+        fern="default",
+        flower_cave="default",
+        wormlights="default",
+
+        --UNPREPARED --options are "never", "rare", "default", "often", "always"
+        berrybush="default",
+        mushroom="default",
+        banana="default",
+        lichen="default",
+
+        --ANIMALS --options are "never", "rare", "default", "often", "always"
+        cave_ponds="default",
+        slurper="default",
+        bunnymen="default",
+        slurtles="default",
+        rocky="default",
+        monkey="default",
+
+        --MONSTERS --options are "never", "rare", "default", "often", "always"
+        cave_spiders="default",
+        tentacles="default",
+        chess="default",
+        liefs="default",
+        bats="default",
+        fissure="default",
+        worms="default"
+    }
 }
